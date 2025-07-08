38b1ff1397f7b1a81fc07576bd3328522cc1feae
Mubashir Zamir <zamir.mubashir@gmail.com>
2025-07-08 20:39:22 +0100

    chore: Added Watch command for experimentations.

---
+++ app/Console/Commands/Watch.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Watch extends Command
+{
+    protected $signature = 'app:watch';
+    protected $description = 'Command description';
+
+    public function handle()
+    {
+
+    }
+}
