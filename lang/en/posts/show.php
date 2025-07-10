11de9a2a0c8972ed9869d247f49f03be27d9a34d
Mubashir Zamir <zamir.mubashir@gmail.com>
2025-07-10 16:50:46 +0100

    feat: Support for multiple strategies i.e. arrays and folders.

--- lang/en/posts/show.php
+++ lang/en/posts/show.php
@@ -5,4 +5,13 @@ return [
     'label_title' => 'Title',
     'label_user_id' => 'User ID',
     'label_content' => 'Content',
+
+
+    'tenant_birch' => [
+      'label_title' => 'Headline',
+    ],
+
+    'tenant_oak' => [
+      'label_title' => 'Heading',
+    ],
 ];
